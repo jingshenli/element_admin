82d97fc79c68d6e94d65e3364c3502bc8727e203
井深丽 <aaa203927@163.com>
2021-11-16 11:31:27 +0800

    第一次

--- .eslintrc.js
+++ .eslintrc.js
@@ -11,7 +11,15 @@ module.exports = {
     parser: 'babel-eslint'
   },
   rules: {
+    // production:线上  需要检测      npm run build   的时候把console.log全部删除
+    // devlopment:线下  不需要检测    npm run serve   console.log 输出数据
+    // "off" 或 0 - 关闭规则
+    // "warn" 或 1 - 开启规则，使用警告级别的错误：warn (不会导致程序退出)
+    // "error" 或 2 - 开启规则，使用错误级别的错误：error (当被触发的时候，程序会退出)
     'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off'
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    'quotes': 0,
+    'space-before-function-paren': 'off'
+    // 'comma-dangle': 1
   }
 }
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <img alt="Vue logo" src="../assets/logo.png" />
+    <HelloWorld msg="Welcome to Your Vue.js App" />
   </div>
 </template>
 
@@ -15,4 +15,4 @@ export default {
     HelloWorld
   }
 }
-</script>
+</script>=
